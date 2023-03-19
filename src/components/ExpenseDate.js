313b79c5f36457e45a032bf0aaaccf124e37597d
Ro <orinarhomill@gmail.com>
2023-03-19 14:45:12 -0500

    Made chagnes to Expense Date JS

---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,17 @@
+import './ExpenseItem.css';
+
+function ExpenseDate(props) {
+  /// Dates are here for the functionality...
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
+
+  return (
+    <div>
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__year">{year}</div>
+      <div className="expense-date__day">{day}</div>
+    </div>
+  );
+}
+export default ExpenseDate;
