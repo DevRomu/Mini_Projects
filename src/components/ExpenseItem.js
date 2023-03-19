57f8ed9ef7038cb3c874508a3c0d09d44c9a0c24
Ro <orinarhomill@gmail.com>
2023-03-19 14:34:54 -0500

    Made changes

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,15 @@
 import "./ExpenseItem.css";
-import ExpenseDate from './ExpenseDate';
+import ExpenseDate from "./ExpenseDate";
 
 function Expenseitem(props) {
-
   // Initial
   return (
     <div className="expense-item">
-         <ExpenseDate date={props.date} />
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <div><h2>{props.title}</h2></div>
+        <div>
+          <h2>{props.title}</h2>
+        </div>
         <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
